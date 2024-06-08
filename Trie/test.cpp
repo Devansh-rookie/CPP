4044f00dfdc0c8e15d78887679d775b031fd2285
Devansh-rookie <devansh151005@gmail.com>
2024-06-08 23:56:21 +0530

    yes

---
+++ Trie/test.cpp
@@ -0,0 +1,11 @@
+// #include<bits/stdc++>
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>> n;
+    int div = n/2;
+    int num = n^(div);
+    cout << num <<endl;
+    return 0;
+}
