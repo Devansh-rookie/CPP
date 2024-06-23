0287311ee99f66cb0f8126a0c790dd5454ee1b70
Devansh-rookie <devansh151005@gmail.com>
2024-06-24 00:54:42 +0530

    graphs 2 and mingw 19

--- graphs/graphs2.cpp
+++ graphs/graphs2.cpp
@@ -1,3 +1,4 @@
+//all the header files included here
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -27,9 +28,88 @@
 #include <typeinfo> 
 #include <utility>
 using namespace std;
+template <typename T>
+
+
+class graph{
+public:
+    // adjacency list
+    unordered_map<T, list<T>> adj; // int is the data type we are using otherwise just use the template type
+    
+    void addNode(T node1, T node2, bool direction){// node1 is connected to node2 by default 1->2 if directed graph else both are connected to each other obviously
+        adj[node1].push_back(node2);
+        if(direction == false){
+            adj[node2].push_back(node1);
+        }
+    }
+
+    void printAdjList(){
+        cout<<endl;
+        for(auto i: adj){
+            cout<< i.first<<" -> ";
+            for(auto j: i.second){
+                cout<< j << ", ";
+            }
+            cout<< endl;
+        }
+        cout<< endl;
+    }
+
+    void undirectedBfs(T vertex){// bfs is to be started from this node
+        unordered_map<T, bool> visited;// or use bool here
+        queue<T> q;
+        cout << endl;
+        q.push(vertex);
+        while(!q.empty()){
+            T currVal = q.front();
+            q.pop();
+            // if not visited
+            if(!visited[currVal]){
+                for(auto i: adj[currVal]){
+                    if(!visited[i]){
+                        q.push(i);
+                    }
+                }
+                cout << currVal << " ";
+                visited[currVal] = true;
+            }
+        }
+        cout<< endl;
+    }
+};
+
 
 int main()
 {
-
+    // e.g. 5 4 0 3 1 3 1 4 1 2
+    // we need sorted way then use set instead and use insert instead of pushback thats all
+    graph<int> g;
+    int n,m;
+    cout << "Give the number of nodes and edges: ";
+    cin >> n >> m;
+    // single nodes means 1 edge as well in this scenario
+    for(int i=0;i< m;i++){
+        int node1, node2;
+        cout << "From where to where:";
+        cin>>node1 >> node2;
+        g.addNode(node1, node2, 0);// undirected graph
+    }
+    g.printAdjList();
+    // g.undirectedBfs(3);
+    // g.undirectedBfs(0);
+    int nVertex;
+    cout << "number of vertex for BFS: ";
+    cin >> nVertex;
+    vector<int> arr(nVertex);
+    cout << "Give all the vertex: ";
+    int x;
+    // useful to print multiple components of the graph
+    for(int i=0;i < nVertex;i++){
+        cin >> x;
+        arr[i] = x;
+    }
+    for(auto vertex: arr){
+        g.undirectedBfs(vertex);
+    }
     return 0;
 }
